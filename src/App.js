2acf2824b41640bbd1fae16f888d5ac432379430
marialobillo <maria.lobillo.santos@gmail.com>
2018-08-03 18:10:37 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD6Uo0XeXsot7vpJSByo340EOcS63KdA9I",
+    authDomain: "bloc-chat-1adce.firebaseapp.com",
+    databaseURL: "https://bloc-chat-1adce.firebaseio.com",
+    projectId: "bloc-chat-1adce",
+    storageBucket: "bloc-chat-1adce.appspot.com",
+    messagingSenderId: "1029508064838"
+  };
+  *.initializeApp(config);
 
 class App extends Component {
   render() {
