92e2c5fc9da85f89e8af18ee3dcf8728968dda2e
marialobillo <maria.lobillo.santos@gmail.com>
2018-08-13 09:59:54 +0200

    formatted date time

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -30,8 +30,36 @@ class MessageList extends Component{
     });
   }
 
+  getDate = (today) => {
+    var day = today.getDate();
+    var month = today.getMonth()+1; //January is 0!
+    var year = today.getFullYear();
+
+    if(day<10) { day = '0'+day }
+    if(month<10) { month = '0'+month }
+
+    let date = month + '/' + day + '/' + year;
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let time = '';
+    if(hours < 12){
+      time = hours + ':' + minutes + 'am';
+    }else{
+      time = (hours - 12) + ':' + minutes + 'pm';
+    }
+    return `${date}, ${time}`;
+  }
+
   addMessage = (e) => {
     console.log(this.state.content);
+    console.log(this.props.activeRoom.key);
+    console.log(this.props.user.displayName);
+
+    let today = new Date();
+    let formattedDate = this.getDate(today);
+    console.log('la otra fecha -> ' + formattedDate);
+
+
     // e.preventDefault();
     // this.messagesRef.push( {
     //   content: this.state.content,
@@ -39,6 +67,7 @@ class MessageList extends Component{
     //   sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
     //   username: this.props.user.displayName
     // });
+    this.setState({ content: ''});
   }
   renderInput = () => {
     return
@@ -47,7 +76,7 @@ class MessageList extends Component{
           className="input-message"
           placeholder="Write a new Message..."
           onChange={this.handleInput}
-          value={this.state.newMessage}
+          value={this.state.content}
         />
         <button className="btn-message"
           onClick={this.addMessage}
@@ -64,6 +93,7 @@ class MessageList extends Component{
           placeholder="Write a new Message..."
           onChange={this.handleInput}
           value={this.state.newMessage}
+          autoFocus
         />
         <button className="btn-message"
           onClick={this.addMessage}
